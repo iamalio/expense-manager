bb23d884956fce4bec9b82059c5491c466de42a6
auswoodall <austin.woodall72@gmail.com>
2018-05-29 14:37:34 -0500

    expense manager

--- src/app/components/app.module.ts
+++ src/app/components/app.module.ts
@@ -2,10 +2,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { SideNavComponent } from './sidenav/sidenav.component';
+import { HeaderComponent } from './header/header.component';
+import { HomeComponent } from './home/home.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SideNavComponent,
+    HeaderComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule
